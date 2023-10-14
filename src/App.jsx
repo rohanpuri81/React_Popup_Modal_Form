a7814ff2fabf689ee38568582d73b50622584cfd
rohanpuri81 <rohanpuri81@gmail.com>
2023-10-14 14:29:48 +0530

    all completed

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,13 @@ import "./App.css";
 
 function App() {
   const [modal, setModal] = useState(false);
+  const [data, setData] = useState([
+    {
+      rating: "4",
+      name: "Rohan Puri",
+      text: "Great UI",
+    },
+  ]);
   const [comment, setComment] = useState({
     rating: "",
     name: "",
@@ -14,6 +21,10 @@ function App() {
     textErr: false,
   });
   function handleInputs(e) {
+    setErrors({
+      nameErr: false,
+      textErr: false,
+    });
     const { name, value } = e.target;
     setComment({
       ...comment,
@@ -23,21 +34,34 @@ function App() {
 
   function handleSubmit(e) {
     e.preventDefault();
-    console.log(comment);
-    console.log(errors);
-    if (comment.name.length <= 2 || comment.name === "") {
+
+    let nam = comment.name;
+    let txt = comment.text;
+    if (nam === "" || (nam.length <= 2 && txt === "" && txt.length <= 6)) {
+      console.log(true);
+      setErrors({
+        ...errors,
+        nameErr: true,
+        textErr: true,
+      });
+    } else if (nam === "" || nam.length <= 2) {
       setErrors({
         ...errors,
         nameErr: true,
       });
-    }
-    if (comment.text.length <= 6) {
+    } else if (txt === "" || txt.length <= 6) {
       setErrors({
         ...errors,
         textErr: true,
       });
+    } else {
+      setData([...data, comment]);
+      setComment({
+        rating: "",
+        name: "",
+        text: "",
+      });
     }
-    // setModal(false);
   }
   return (
     <div className="App">
@@ -52,7 +76,12 @@ function App() {
                 <div>
                   <label htmlFor="rating">Rating</label>
                   <br />
-                  <select name="rating" id="rating" onChange={handleInputs}>
+                  <select
+                    name="rating"
+                    id="rating"
+                    onChange={handleInputs}
+                    value={comment.rating}
+                  >
                     <option value="1">1</option>
                     <option value="2">2</option>
                     <option value="3">3</option>
@@ -70,6 +99,7 @@ function App() {
                     type="text"
                     name="name"
                     id="name"
+                    value={comment.name}
                     className="form-control"
                     placeholder="Enter Your Name"
                   />
@@ -87,6 +117,7 @@ function App() {
                     onChange={handleInputs}
                     name="text"
                     id="comment"
+                    value={comment.text}
                     className="form-control"
                     placeholder="Enter text here..."
                     style={{ resize: "none" }}
@@ -109,6 +140,19 @@ function App() {
         </ModalBody>
       </Modal>
       <button onClick={() => setModal(true)}>Add Comment</button>
+
+      <div className="div">
+        <h2>All Comments</h2>
+        {data.map((cm, ele) => {
+          return (
+            <div key={ele} className="inner">
+              <h3>Name: {cm.name}</h3>
+              <h5>comment : {cm.text}</h5>
+              <h5>rating : {cm.rating}</h5>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
